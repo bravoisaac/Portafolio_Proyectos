e65953062a6352c6290a2c406c59cc502d9fbf78
isaac bravo <isaacbravo1431@gmail.com>
2026-06-08 19:01:19 -0400

    08/06/2026

---
+++ Portafolio_Proyectos/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Portafolio_Proyectos',
+  webDir: 'www'
+};
+
+export default config;
